b36c488fa68155ce687ac40b56dd92bd92f49aaa
SaltyAom <aomkirby123@gmail.com>
2021-07-08 20:44:58 +0700

    chore(app): rand

--- code/src/services/random/index.ts
+++ code/src/services/random/index.ts
@@ -1,3 +1,3 @@
 // ? Event random distribution as Math.random can't be 1.
-export const random = (min: number, max: number) => Math.floor(Math.random() * max + min + 1)
-export const randomPick = <T>(array: T[]): T => array[random(0, array.length - 1)]
+export const random = (min: number, max: number) => Math.floor(Math.random() * max + min)
+export const randomPick = <T>(array: T[]): T => array[random(0, array.length)]
